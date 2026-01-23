11479b6efb71814d74113db8442a3db5e4b32a07
白彩恋 <shiro@oom-wg.dev>
2026-01-23 22:33:11 +0800

    Update source.tsx

--- src/lib/source.tsx
+++ src/lib/source.tsx
@@ -137,7 +137,15 @@ export const docsOptions = {
 	nav: {
 		title: (
 			<div className='flex items-center gap-2'>
-				<Image alt='Logo' src='/icon' width={32} height={32} className='rounded-md' unoptimized />
+				<Image
+					alt='Logo'
+					src='/icon'
+					width={32}
+					height={32}
+					className='rounded-md'
+					priority
+					unoptimized
+				/>
 				<span className='font-semibold'>{docsConfig.title}</span>
 			</div>
 		)
