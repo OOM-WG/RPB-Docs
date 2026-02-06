af4a3a9a3f1c6fa17c06ef860f439186f370a2ab
白彩恋 <shiro@oom-wg.dev>
2026-02-06 10:00:45 +0800

    AD

--- src/lib/source.tsx
+++ src/lib/source.tsx
@@ -68,6 +68,10 @@ export const docsConfig = {
 				title: '友链',
 				items: [
 					{
+						label: '广告',
+						href: 'https://ja7.top/ad'
+					},
+					{
 						label: 'Latest File',
 						href: 'https://latestfile.zip'
 					},
