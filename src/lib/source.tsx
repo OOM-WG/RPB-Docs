6e6eae005b0837d2d2975aed6c07afae9d962d3e
白彩恋 <shiro@oom-wg.dev>
2026-06-12 14:54:46 +0800

    Update License
    Signed-off-by: 白彩恋 <shiro@oom-wg.dev>

--- src/lib/source.tsx
+++ src/lib/source.tsx
@@ -90,7 +90,7 @@ export const docsConfig = {
 			<span>
 				根据{' '}
 				<Link href='https://license.fileto.download/' target='_blank'>
-					F2DLPR License
+					File to Downloader 许可证
 				</Link>{' '}
 				授权许可。©️ 2016-{new Date().getFullYear()}{' '}
 				<Link href='https://shiror.ren/' target='_blank'>
